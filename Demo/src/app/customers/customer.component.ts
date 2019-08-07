bd110a9dc4e4685584e965d0c10555e43eecb216
Rafael Walter <rafawalter@gmail.com>
2019-08-07 16:49:04 -0300

    Custom Validators with parameters

--- Demo/src/app/customers/customer.component.ts
+++ Demo/src/app/customers/customer.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, FormBuilder, Validators, AbstractControl} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn} from '@angular/forms';
 
-import { Customer } from './customer';
+import {Customer} from './customer';
 
-function ratingRange(c: AbstractControl): {[key: string]: boolean} | null {
-  if (c.value !== null && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
-    return { 'range': true };
-  }
-  return null;
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return {'range': true};
+    }
+    return null;
+  };
 }
 
 @Component({
@@ -21,16 +23,17 @@ export class CustomerComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
-      lastName: ['',[Validators.required, Validators.maxLength(50)]],
-      email: ['',[Validators.required, Validators.email]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
       phone: [''],
       notification: ['email'],
-      rating: [null, ratingRange],
+      rating: [null, ratingRange(1, 5)],
       sendCatalog: true
     });
   }
