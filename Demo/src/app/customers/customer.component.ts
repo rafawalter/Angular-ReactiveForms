ac99f28e1d86f26fab605fb5a692668e0b33aaf2
Rafael Walter <rafawalter@gmail.com>
2019-08-07 17:46:38 -0300

    Reacting to changes: Watching

--- Demo/src/app/customers/customer.component.ts
+++ Demo/src/app/customers/customer.component.ts
@@ -54,6 +54,10 @@ export class CustomerComponent implements OnInit {
       rating: [null, ratingRange(1, 5)],
       sendCatalog: true
     });
+
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => console.log(value)
+    );
   }
 
   save() {
