a441ed7abc34b61d2b91c0998be1d5cad463cd15
Rafael Walter <rafawalter@gmail.com>
2019-08-08 11:08:15 -0300

    Dynamically duplicate Input Elements: Define the input elements to duplicate

--- Demo/src/app/customers/customer.component.ts
+++ Demo/src/app/customers/customer.component.ts
@@ -59,7 +59,13 @@ export class CustomerComponent implements OnInit {
       phone: [''],
       notification: ['email'],
       rating: [null, ratingRange(1, 5)],
-      sendCatalog: true
+      sendCatalog: true,
+      addressType: 'home',
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zip: '',
     });
 
     this.customerForm.get('notification').valueChanges.subscribe(
